b969e524b62d66c9a54c7415815f01ea7ce82349
Káplár István <pistike.kaplar@gmail.com>
2021-03-14 12:49:49 +0100

    2

--- src/main/java/countries/Homework1.java
+++ src/main/java/countries/Homework1.java
@@ -28,8 +28,8 @@ public class Homework1 {
      *  Returns the first country name that contains the word "island" ignoring case.
      */
     public Optional<String> streamPipeline2() {
-        // TODO
-        return null;
+        return countries.stream().map(Country::getName).filter(nev -> nev.toLowerCase().contains("island")).findFirst();
+
     }
 
     /**
